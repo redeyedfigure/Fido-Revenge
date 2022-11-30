44ec6c6ac7a6c882ee34cab1acbacc4957fdfd6e
Redeyedfigure <dmarsh735@gmail.com>
2022-11-30 08:17:21 +0700

    Shooting Script

--- Fido Revenge/Assets/Scripts/Player/Shooting.cs
+++ Fido Revenge/Assets/Scripts/Player/Shooting.cs
@@ -47,16 +47,12 @@ public class Shooting : MonoBehaviour
     private WaitForSeconds shotDuration = new WaitForSeconds(0.07f);
 
     //Accessible GameObjects
-    private Camera camera;
-    private Transform gunEnd;
+    public Transform gunEnd;
+    private new Camera camera;
     private AudioSource gunAudio;
-    private LineRenderer laserLine;
 
     void Start()
     {
-        //Garner access to LineRenderer Component
-        laserLine = GetComponent<LineRenderer>();
-
         //Garner access to AudioSource Component
         gunAudio = GetComponent<AudioSource>();
 
@@ -68,7 +64,34 @@ public class Shooting : MonoBehaviour
     {
         if(Input.GetButtonDown("Fire1") && Time.time > nextFire)
         {
+            //Update the time when our player can fire next
             nextFire = Time.time + fireRate;
+
+            //Begins effects loop
+            StartCoroutine (ShotEffect());
+
+            //Create a vector at the center of camera's viewport
+            Vector3 rayOrigin = camera.ViewportToWorldPoint (new Vector3(0.5f, 0.5f, 0.0f));
+
+            //Declare raycast hit
+            RaycastHit hit;
+
+            //Check if raycast has hit anything
+            if(Physics.Raycast (rayOrigin,camera.transform.forward, out hit, weaponRange))
+            {
+                
+            }
+
         }
+
+    }
+
+    private IEnumerator ShotEffect()
+    {
+        //Plays the sound effect
+        gunAudio.Play();
+
+        //Wait for .07 seconds
+        yield return shotDuration;
     }
 }
